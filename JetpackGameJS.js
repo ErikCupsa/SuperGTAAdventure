1b2d99a7bfeaaee5c9c4367653d31b7cb3afaa60
Erik <50710474+ErikCupsa@users.noreply.github.com>
2019-06-06 09:39:36 -0400

    Update JetpackGameJS.js

--- JetpackGameJS.js
+++ JetpackGameJS.js
@@ -1,11 +1,13 @@
 var userCharacter;
-var generatedObstacles = [];
+var generatedObstaclesTop = [];
+var generatedObstaclesBottom = [];
 var userScore;
 var userHighScore = 0;
 var displayHighScore;
 var BackgroundMusicJetpack;
 var deathNoise;
 var jetpackNoise;
+var userLevel = 1;
 
 function startJetpackGame() {
   BackgroundMusicJetpack = new sound("BackgroundMusicJetpack.mp3");
@@ -16,8 +18,9 @@ function startJetpackGame() {
   displayHighScore = document.createElement("p");
   displayHighScore.id = "HighScore";
   document.body.appendChild(displayHighScore);
-  document.getElementById("HighScore").innerHTML = "Your high score is " + userHighScore;
+  document.getElementById("HighScore").innerHTML = "Your high score is " + userHighScore + ". You are currently on level " + userLevel;
   gameArea.start();
+  document.getElementById("start").disabled = true;
 }
 
 var gameArea = {
@@ -54,21 +57,21 @@ function component(width, height, color, x, y, type) {
     this.gravity = 0.05;
     this.gravitySpeed = 0;
     this.update = function() {
-        ctx = gameArea.context;
+        context = gameArea.context;
         if (this.type == "text") {
-            ctx.font = this.width + " " + this.height;
-            ctx.fillStyle = color;
-            ctx.fillText(this.text, this.x, this.y);
+            context.font = this.width + " " + this.height;
+            context.fillStyle = color;
+            context.fillText(this.text, this.x, this.y);
         }
         else if (type == "image") {
-            ctx.drawImage(this.image,
+            context.drawImage(this.image,
             this.x,
             this.y,
             this.width, this.height);
           }
           else {
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
+            context.fillStyle = color;
+            context.fillRect(this.x, this.y, this.width, this.height);
         }
     }
     this.newPosition = function() {
@@ -105,8 +108,15 @@ function component(width, height, color, x, y, type) {
 
 function updateGameArea() {
     var x, height, gap, minHeight, maxHeight, minGap, maxGap;
-    for (i = 0; i < generatedObstacles.length; i += 1) {
-        if (userCharacter.crashWith(generatedObstacles[i]) || this.y > (gameArea.canvas.height - this.height) || this.y < 0) {
+    for (i = 0; i < generatedObstaclesTop.length; i += 1) {
+        if (userCharacter.crashWith(generatedObstaclesTop[i]) || userCharacter.crashWith(generatedObstaclesBottom[i]) || this.y > (gameArea.canvas.height - this.height) || this.y < 0) {
+          stop();
+          restartGame();
+          return;
+        }
+    }
+    for (i = 0; i < generatedObstaclesBottom.length; i += 1) {
+        if (userCharacter.crashWith(generatedObstaclesTop[i]) || userCharacter.crashWith(generatedObstaclesBottom[i]) || this.y > (gameArea.canvas.height - this.height) || this.y < 0) {
           stop();
           restartGame();
           return;
@@ -115,24 +125,48 @@ function updateGameArea() {
     gameArea.clear();
     gameArea.frameNo += 1;
     if (gameArea.frameNo == 1 || everyinterval(75)) {
+      var building;
+      var floornumber = Math.round(Math.random()*3);
+      if(floornumber == 1){
+        building = "Building1.png";
+      }
+      else if(floornumber == 2){
+        building = "Building2.png";
+      }
+      else{
+        building = "Building2.png";
+      }
+      var air;
         x = gameArea.canvas.width;
         minHeight = 20;
         maxHeight = 200;
         height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+        if (height < 75){
+          air = "Plane.png";
+        }
+        else if(height < 125){
+          air = "Blimp.png";
+        }
+        else {
+          air = "Helicopter.png";
+        }
+
         minGap = 75;
         maxGap = 100;
         gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-        generatedObstacles.push(new component(10, height, "gold", x, 0));
-        generatedObstacles.push(new component(10, x - height - gap, "goldenrod", x, height + gap));
+        generatedObstaclesTop.push(new component(100, height, air, x, 0, "image"));
+        generatedObstaclesBottom.push(new component(100, x - height - gap, building, x, height + gap, "image"));
     }
-    for (i = 0; i < generatedObstacles.length; i += 1) {
-        generatedObstacles[i].x += -3;
-        generatedObstacles[i].update();
+    if (userLevel == 1){
+      levelOne();
+    }
+    else if (userLevel == 2) {
+      levelTwo();
     }
     userScore.text="SCORE: " + gameArea.frameNo;
     if (gameArea.frameNo > userHighScore){
       userHighScore = gameArea.frameNo;
-      document.getElementById("HighScore").innerHTML = "Your high score is " + userHighScore;
+      document.getElementById("HighScore").innerHTML = "Your high score is " + userHighScore + ". You are currently on level " + userLevel;;
     }
     userScore.update();
     userCharacter.newPosition();
@@ -155,6 +189,18 @@ function moveGamePiece(event) {
       accelerate(-0.4);
    userCharacter.image.src = "Character1MFlying.png"
  }
+ else if (keyPressed == "8") {
+   alert("You gave up on level " + userLevel);
+   userLevel++;
+   gameArea.frameNo = 0;
+   userHighScore = 0;
+   updateGameArea();
+   restartGame();
+ }
+ else {
+   userCharacter.speedY = 3;
+    accelerate(0.4);
+ }
 }
 function resetMovement(){
   jetpackNoise.stop();
@@ -169,7 +215,8 @@ function restartGame() {
   if (gameArea.frameNo > userHighScore) {
     userHighScore = gameArea.frameNo;
   }
-  generatedObstacles = [];
+  generatedObstaclesTop = [];
+  generatedObstaclesBottom = [];
   gameArea.frameNo = 0;
   userCharacter = new component(20, 30, "Character1M.png", 10, 150, "image");
 }
@@ -186,4 +233,68 @@ function sound(src) {
     this.stop = function(){
       this.sound.pause();
     }
+}
+function levelOne(){
+  for (i = 0; i < generatedObstaclesTop.length; i++) {
+      generatedObstaclesTop[i].x += -3;
+      generatedObstaclesTop[i].update(
+      );
+      if (gameArea.frameNo % 50 == 0){
+        generatedObstaclesTop[i].y = generatedObstaclesTop[i].y + 5;
+        generatedObstaclesTop[i].update();
+      }
+      else if(gameArea.frameNo % 25 == 0){
+        generatedObstaclesTop[i].y = generatedObstaclesTop[i].y -5;
+        generatedObstaclesTop[i].update();
+      }
+      if(gameArea.frameNo > 100){
+        generatedObstaclesTop[i].x += -3;
+        generatedObstaclesTop[i].update();
+      }
+  }
+  for (i = 0; i < generatedObstaclesBottom.length; i++) {
+      generatedObstaclesBottom[i].x += -3;
+      generatedObstaclesBottom[i].update(
+      );
+      if (gameArea.frameNo > 100){
+        generatedObstaclesBottom[i].x += -3;
+        generatedObstaclesBottom[i].update();
+      }
+  }
+  if(gameArea.frameNo > 100) {
+    userLevel = 2;
+    gameArea.frameNo = 0;
+    userHighScore = 0;
+    updateGameArea();
+    restartGame();
+  }
+}
+function levelTwo(){
+  document.getElementById("HighScore").innerHTML = "Your high score is " + userHighScore + ". You are currently on level " + userLevel;
+  for (i = 0; i < generatedObstaclesTop.length; i++) {
+      generatedObstaclesTop[i].x += -3;
+      generatedObstaclesTop[i].update(
+      );
+      if (gameArea.frameNo % 50 == 0){
+        generatedObstaclesTop[i].y = generatedObstaclesTop[i].y + 5;
+        generatedObstaclesTop[i].update();
+      }
+      else if(gameArea.frameNo % 25 == 0){
+        generatedObstaclesTop[i].y = generatedObstaclesTop[i].y -5;
+        generatedObstaclesTop[i].update();
+      }
+      if(gameArea.frameNo > 100){
+        generatedObstaclesTop[i].x += -3;
+        generatedObstaclesTop[i].update();
+      }
+  }
+  for (i = 0; i < generatedObstaclesBottom.length; i++) {
+      generatedObstaclesBottom[i].x += -3;
+      generatedObstaclesBottom[i].update(
+      );
+      if (gameArea.frameNo > 100){
+        generatedObstaclesBottom[i].x += -3;
+        generatedObstaclesBottom[i].update();
+      }
+  }
 }
